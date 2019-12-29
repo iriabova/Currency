3161836f4af0ad4e7a17cfecd427feab3fcb1a45
iriabova <julia.rabova1980@gmail.com>
2019-12-28 23:09:27 -0500

    currency test case

---
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -0,0 +1,340 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Features/MyCases3.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Tek School Scenarios3",
+  "description": "",
+  "id": "tek-school-scenarios3",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 4243032700,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User is on TekSchool page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "User Click on Test Environment link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "User should see Test Environment page",
+  "keyword": "And "
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 9,
+      "value": "#Scenario: SDET2019AUG_SDET_003_Execution"
+    }
+  ],
+  "line": 10,
+  "name": "User click on my account menu on top of the page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "User click on login on my account menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "User enter username \u0027teststudent@gmail.com\u0027 and password \u0027Tek@Test.com\u0027 in returning menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User click on login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "User should be logged in",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_TekSchool_page()"
+});
+formatter.result({
+  "duration": 3634638200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_Click_on_Test_Environment_link()"
+});
+formatter.result({
+  "duration": 2071605100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_should_see_Test_Environment_page()"
+});
+formatter.result({
+  "duration": 18686600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_click_on_my_account_menu_on_top_of_the_page()"
+});
+formatter.result({
+  "duration": 547657000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_click_on_login_on_my_account_menu()"
+});
+formatter.result({
+  "duration": 395117100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "teststudent@gmail.com",
+      "offset": 21
+    },
+    {
+      "val": "Tek@Test.com",
+      "offset": 58
+    }
+  ],
+  "location": "TekSchoolScenariosStepDefinitions.user_enter_username_username_and_password_password_in_returning_menu(String,String)"
+});
+formatter.result({
+  "duration": 245718800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_click_on_login_button()"
+});
+formatter.result({
+  "duration": 982518900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_should_be_logged_in()"
+});
+formatter.result({
+  "duration": 447814200,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "SDET_O9_Currency",
+  "description": "",
+  "id": "tek-school-scenarios3;sdet-o9-currency",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 16,
+      "name": "@SDET_09"
+    }
+  ]
+});
+formatter.step({
+  "line": 18,
+  "name": "User ckics on currensy tap on the lefy top of the page",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "tree currensies are displayed",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "User can click on different currency",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 21,
+  "name": "choosen currency is displayed under the products",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 22,
+  "name": "user clicks on phones and PDAs",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "user clicks on spesific pruduct",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "User should see the chosen currency is displayed on the product ditails page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 25,
+  "name": "User clicks on Add to Cart Button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 26,
+  "name": "User sees the chesen currensy is displayed",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "User clicks on my account Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "User clicks on log out Button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 29,
+  "name": "user clicks on my account menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "User click on login on my account menu1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "User enter username \u0027teststudent@gmail.com\u0027 and password \u0027Tek@Test.com\u0027 in returning menu1",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "User click on login button1",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_ckics_on_currensy_tap_on_the_lefy_top_of_the_page()"
+});
+formatter.result({
+  "duration": 72053800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.tree_currensies_are_displayed()"
+});
+formatter.result({
+  "duration": 24100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_can_click_on_different_currency()"
+});
+formatter.result({
+  "duration": 427032300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.choosen_currency_is_displayed_under_the_products()"
+});
+formatter.result({
+  "duration": 369015000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_clicks_on_phones_and_PDAs()"
+});
+formatter.result({
+  "duration": 594486800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_clicks_on_spesific_pruduct()"
+});
+formatter.result({
+  "duration": 1418987900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_should_see_the_chosen_currency_is_displayed_on_the_product_ditails_page()"
+});
+formatter.result({
+  "duration": 412407900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_clicks_on_Add_to_Cart_Button()"
+});
+formatter.result({
+  "duration": 76492600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_sees_the_chesen_currensy_is_displayed()"
+});
+formatter.result({
+  "duration": 375450400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_clicks_on_my_account_Button()"
+});
+formatter.result({
+  "duration": 67541900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_clicks_on_log_out_Button()"
+});
+formatter.result({
+  "duration": 486100000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_clicks_on_my_account_menu()"
+});
+formatter.result({
+  "duration": 67804400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_click_on_login_on_my_account_menu1()"
+});
+formatter.result({
+  "duration": 283389100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "teststudent@gmail.com",
+      "offset": 21
+    },
+    {
+      "val": "Tek@Test.com",
+      "offset": 58
+    }
+  ],
+  "location": "TekSchoolScenariosStepDefinitions.user_enter_username_username_and_password_password_in_returning_menu1(String,String)"
+});
+formatter.result({
+  "duration": 213966200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TekSchoolScenariosStepDefinitions.user_click_on_login_button1()"
+});
+formatter.result({
+  "duration": 845403400,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 716197800,
+  "status": "passed"
+});
+});
